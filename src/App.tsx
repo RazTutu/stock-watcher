065b75f2b7d066bec53395894faab4067cc449bd
RazTutu <raztutu123@gmail.com>
2022-12-04 20:45:50 +0200

    create custom hook useStocks to fetch all stocks for a symbol

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import { useStocks } from './hooks/useStocks';
 import './App.css'
 
 function App() {
-
+  const stocks = useStocks('aapl');
+  console.log('stocks are', stocks);
   return (
     <div className="App">
       <p>basic text</p>
---
+++ src/hooks/useStocks.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export function useStocks(symbol: string) {
+  const apiKey = "9BVUEI3B52GEA2R8";
+  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&outputsize=full&apikey=${apiKey}`
+  const { data } = useQuery(["stocks", symbol], () =>
+    axios.get(url), 
+    {
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+    }
+  );
+  return data;
+}
